0a73ac7c6ee8fdf686d4a19cff936b14967af2ff
Diego Bahena <dbahenalopez@hotmail.com>
2024-04-23 11:03:01 -0600

    Added Queue data structure

--- P2/queue.c
+++ P2/queue.c
@@ -11,29 +11,54 @@ typedef nodeType *pNode;
 typedef nodeType *queue;
 
 
+void push (pNode *first, pNode *last, int data);
+int pop (pNode *first, pNode *last);
+void removeElements (pNode *first, pNode *last, int numberOfElements);
+
+int main (){
+
+    pNode first = NULL, last = NULL;
+
+    int input;
+
+    push(&first, &last, 10);
+    push(&first, &last, 20);
+    push(&first, &last, 30);
+    push(&first, &last, 40);    
+    push(&first, &last, 50);
+    push(&first, &last, 60);
+
+    pop(&first, &last);
+
+    scanf("%d", &input);
+
+    removeElements(&first, &last, input);
+
+    return 0;
+}
 
 
-void add (pNode *first, pNode *last, int data){
+void push (pNode *first, pNode *last, int data){
 
 
     pNode new;
 
     new = (pNode) malloc(sizeof(nodeType));
 
-    new.datadata = data;
-    new.next = NULL;
+    new->data = data;
+    new->next = NULL;
 
     if (*last)
-        *last.next = new;
-    else
-        *last = new; 
+        (*last)->next = new;
+    
+    *last = new; 
 
     if (!*first)
         *first = new;
 
 }
 
-int read (pNode *first, pNode *last){
+int pop (pNode *first, pNode *last){
     
     pNode node;
 
@@ -44,11 +69,39 @@ int read (pNode *first, pNode *last){
     if (!node)
         return 0;
 
-    first = node->next;
+    *first = node->next;
 
     data = node->data;
 
     free(node);
 
+    if(! *first)
+        *last = NULL;
+
     return data;
+}
+
+void removeElements (pNode *first, pNode *last, int numberOfElements){
+    
+    pNode node;
+
+    for (int i = 0; i < numberOfElements; i++){
+        
+        if (! *first){
+            printf("Queue is empty\n");
+
+            return;
+        }
+ 
+        node = *first;
+        
+        *first = node->next;
+
+        printf("%d\n", node->data);
+
+        free(node);
+    }    
+
+    if(! *first)
+        *last = NULL;
 }
