df32f57a34eba9d8f3eaf30168d40a2a07bfb05d
DiegoErnestoBahenaLopez <dbahenalopez@hotmail.com>
2024-06-10 07:28:56 -0600

    Improved binary search tree

--- P3/binarySearchTree.c
+++ P3/binarySearchTree.c
@@ -13,345 +13,449 @@ typedef struct node {
 typedef nodeType *pNode;
 typedef nodeType *Tree;
 
-/* Funciones con �rboles: */
-/* Insertar en �rbol ordenado: */
-void Insertar(Tree *a, int dat);
-/* Borrar un elemento: */
-void Borrar(Tree *a, int dat);
-/* Funci�n de b�squeda: */
-int Buscar(Tree a, int dat);
-/* Comprobar si el �rbol est� vac�o: */
-int Vacio(Tree r);
-/* Comprobar si es un nodo hoja: */
-int EsHoja(pNode r);
-/* Contar n�mero de nodos: */
-int NumeroNodos(Tree a, int* c);
-/* Calcular la altura de un �rbol: */
-int AlturaTree(Tree a, int* altura);
-/* Calcular altura de un value: */
-int Altura(Tree a, int dat);
-/* Aplicar una funci�n a cada elemento del �rbol: */
-void InOrden(Tree, void (*func)(int*));
-void PreOrden(Tree, void (*func)(int*));
-void PostOrden(Tree, void (*func)(int*));
 
-/* Funciones auxiliares: */
-void Podar(Tree *a);
-void auxContador(Tree a, int*);
-void auxAltura(Tree a, int, int*);
+void Insert(Tree *tree, int data);
+void Delete(Tree *tree, int data);
+int Search(Tree tree, int data);
+void Prune(Tree *tree);
 
-void Mostrar(int *d);
+void InOrder(Tree, void (*func)(int*));
+void PreOrder(Tree, void (*func)(int*));
+void PostOrder(Tree, void (*func)(int*));
 
-/* Programa de ejemplo */
+int NumberOfNodes(Tree tree, int* count);
+int TreeHeight(Tree tree, int* height);
+int Height(Tree tree, int data);
+
+int IsEmpty(Tree tree);
+int IsLeaf(pNode node);
+void auxCounter(Tree a, int*);
+void auxHeight(Tree a, int, int*);
+void Show(int *data);
+
 int main()
 {
-   Tree TreeInt=NULL;
-   int altura;
-   int nnodos;
+   Tree tree = NULL;
+   int height;
+   int numberOfNodes;
+   int nodeHeight;
 
-   /* Inserci�n de nodos en �rbol: */
-   Insertar(&TreeInt, 10);
-   Insertar(&TreeInt, 5);
-   Insertar(&TreeInt, 12);
-   Insertar(&TreeInt, 4);
-   Insertar(&TreeInt, 7);
-   Insertar(&TreeInt, 3);
-   Insertar(&TreeInt, 6);
-   Insertar(&TreeInt, 9);
-   Insertar(&TreeInt, 8);
-   Insertar(&TreeInt, 11);
-   Insertar(&TreeInt, 14);
-   Insertar(&TreeInt, 13);
-   Insertar(&TreeInt, 2);
-   Insertar(&TreeInt, 1);
-   Insertar(&TreeInt, 15);
-   Insertar(&TreeInt, 10);
-   Insertar(&TreeInt, 17);
-   Insertar(&TreeInt, 18);
-   Insertar(&TreeInt, 16);
+   int option = 0, input;
 
-   printf("Altura de Tree %d\n", AlturaTree(TreeInt, &altura));
 
-   /* Mostrar el �rbol en tres ordenes distintos: */
-   printf("InOrden: ");
-   InOrden(TreeInt, Mostrar);
-   printf("\n");
-   printf("PreOrden: ");
-   PreOrden(TreeInt, Mostrar);
-   printf("\n");
-   printf("PostOrden: ");
-   PostOrden(TreeInt, Mostrar);
-   printf("\n");
+    while (option != 8){
 
-   /* Borraremos algunos elementos: */
-   printf("N nodos: %d\n", NumeroNodos(TreeInt, &nnodos));
-   Borrar(&TreeInt, 5);
-   printf("Borrar 5: ");
-   InOrden(TreeInt, Mostrar);
-   printf("\n");
-   Borrar(&TreeInt, 8);
-   printf("Borrar 8: ");
-   InOrden(TreeInt, Mostrar);
-   printf("\n");
-   Borrar(&TreeInt, 15);
-   printf("Borrar 15: ");
-   InOrden(TreeInt, Mostrar);
-   printf("\n");
-   Borrar(&TreeInt, 245);
-   printf("Borrar 245: ");
-   InOrden(TreeInt, Mostrar);
-   printf("\n");
-   Borrar(&TreeInt, 4);
-   printf("Borrar 4: ");
-   InOrden(TreeInt, Mostrar);
-   printf("\n");
-   Borrar(&TreeInt, 17);
-   printf("Borrar 17: ");
-   InOrden(TreeInt, Mostrar);
-   printf("\n");
+        printf("\n1.Insert Nodes\n2.Show\n3.Delete\n4.Prune\n5.Number of nodes\n6.Tree Height\n7.Node Height\n8.Exit\n");
 
-   /* Veamos algunos par�metros */
-   printf("N nodos: %d\n", NumeroNodos(TreeInt, &nnodos));
-   printf("Altura de 1 %d\n", Altura(TreeInt, 1));
-   printf("Altura de 10 %d\n", Altura(TreeInt, 10));
-   printf("Altura de Tree %d\n", AlturaTree(TreeInt, &altura));
+        scanf("%d",&option);
 
-   /* Liberar memoria asociada al �rbol: */
-   Podar(&TreeInt);
-   system("PAUSE");
+        switch (option)
+        {
+            case 1:
+
+               int nodesToCreate;
+
+               printf("Enter the number of nodes:\n");
+
+               scanf("%d", &nodesToCreate);
+
+               printf("Enter the values of the nodes:\n");
+
+               for (int i = 0; i < nodesToCreate; i++){
+                  printf("Node %d :\n", i + 1);
+
+                  scanf("%d", &input);
+
+                  Insert(&tree, input);
+               }
+
+            break;
+
+            case 2: 
+
+               if(IsEmpty(tree)){
+                  printf("The tree is empty");
+               }
+               else {
+
+                  int showOption;
+                  printf("\n1.In Order\n2.Pre Order\n3.Post Order\n");
+
+                  scanf("%d", &showOption);
+
+                  switch (showOption)
+                  {
+                     case 1:
+                     
+                        InOrder(tree, Show);
+
+                     break;
+
+                     case 2:
+                     
+                        PreOrder(tree, Show);
+
+                     break;
+
+                     case 3:
+                     
+                        PostOrder(tree, Show);
+
+                     break;
+                  
+                     default:
+                        printf("Enter a valid option\n");
+                     break;
+                  }
+               } 
+
+               
+
+            break;
+
+            case 3:
+               if(IsEmpty(tree)){
+                  printf("The tree is empty");
+               }
+               else {
+                  printf("Enter the element to delete:\n");
+
+                  scanf("%d", &input);
+
+                  Delete(&tree, input);
+               }
+
+              
+            break;
+
+            case 4:
+
+               if(IsEmpty(tree)){
+                  printf("The tree is empty");
+               }
+               else {
+                  Prune(&tree);
+               }  
+            break;
+            
+            case 5:
+               if(IsEmpty(tree)){
+                  printf("The tree is empty");
+               }
+               else {
+                  printf("The number of nodes is: %d\n",NumberOfNodes(tree, &numberOfNodes));
+               }
+                      
+            break;
+
+            case 6:
+
+               if(IsEmpty(tree)){
+                  printf("The tree is empty");
+               }
+               else {
+                  printf("The tree height is: %d\n",TreeHeight(tree, &height));
+               }
+
+            break;
+
+            case 7:
+               
+               if(IsEmpty(tree)){
+                  printf("The tree is empty");
+               }
+               else {
+                  printf("Enter the element to determine height:\n");
+
+                  scanf("%d", &input);
+
+                  printf("The element height is: %d\n", Height(tree, input));
+               }
+            break;
+
+            case 8:
+
+            break;
+
+            default:
+                printf("Enter a valid option.\n");       
+            break;
+        }
+    }
+
+  
    return 0;
 }
 
-/* Poda: borrar todos los nodos a partir de uno, incluido */
-void Podar(Tree *a)
-{
-   /* Algoritmo recursivo, recorrido en postorden */
-   if(*a) {
-      Podar(&(*a)->left); /* Podar left */
-      Podar(&(*a)->right);   /* Podar right */
-      free(*a);                /* Eliminar nodo */
-      *a = NULL;
+void Prune(Tree *tree)
+{ 
+   if(*tree) {
+
+      Prune(&(*tree)->left); 
+      Prune(&(*tree)->right);   
+     
+      free(*tree);                
+      *tree = NULL;
    }
 }
 
-/* Insertar un value en el �rbol ABB */
-void Insertar(Tree *a, int dat)
+void Insert(Tree *tree, int data)
 {
-   pNode padre = NULL;
-   pNode actual = *a;
+   pNode parent = NULL;
+   pNode current = *tree;
 
-   /* Buscar el value en el �rbol, manteniendo un puntero al nodo padre */
-   while(!Vacio(actual) && dat != actual->value) {
-      padre = actual;
-      if(dat < actual->value) actual = actual->left;
-      else if(dat > actual->value) actual = actual->right;
+   //Search for the value to insert in the tree, keeping a pointer to the parent node
+   while(!IsEmpty(current) && data != current->value) {
+      parent = current;
+
+      if(data < current->value) 
+         current = current->left;
+
+      else if(data > current->value) 
+         current = current->right;
    }
 
-   /* Si se ha encontrado el elemento, regresar sin insertar */
-   if(!Vacio(actual)) return;
-   /* Si padre es NULL, entonces el �rbol estaba vac�o, el nuevo nodo ser�
-      el nodo raiz */
-   if(Vacio(padre)) {
-      *a = (Tree)malloc(sizeof(nodeType));
-      (*a)->value = dat;
-      (*a)->left = (*a)->right = NULL;
+   //If element found, return
+   if(!IsEmpty(current)) 
+      return;
+
+
+   //If parent is null, then the tree was empty, the new node will be the root
+   if(IsEmpty(parent)) {
+
+      *tree = (Tree)malloc(sizeof(nodeType));  
+      (*tree)->value = data;
+      (*tree)->left = (*tree)->right = NULL;
    }
-   /* Si el value es menor que el que contiene el nodo padre, lo insertamos
-      en la rama izquierda */
-   else if(dat < padre->value) {
-      actual = (Tree)malloc(sizeof(nodeType));
-      padre->left = actual;
-      actual->value = dat;
-      actual->left = actual->right = NULL;
+
+   //If the value to insert is lesser than the parent node, it 
+   //is inserted in the left branch 
+   else if(data < parent->value) {
+      current = (Tree)malloc(sizeof(nodeType));
+      parent->left = current;
+      current->value = data;
+      current->left = current->right = NULL;
    }
-   /* Si el value es mayor que el que contiene el nodo padre, lo insertamos
-      en la rama derecha */
-   else if(dat > padre->value) {
-      actual = (Tree)malloc(sizeof(nodeType));
-      padre->right = actual;
-      actual->value = dat;
-      actual->left = actual->right = NULL;
+
+   //If the value to insert is greater than the parent node, it
+   //is inserted in the right branch
+   else if(data > parent->value) {
+      current = (Tree)malloc(sizeof(nodeType));
+      parent->right = current;
+      current->value = data;
+      current->left = current->right = NULL;
   }
 }
 
-/* Eliminar un elemento de un �rbol ABB */
-void Borrar(Tree *a, int dat)
+void Delete(Tree *tree, int data)
 {
-   pNode padre = NULL;
-   pNode actual;
-   pNode nodo;
+   pNode parent = NULL;
+   pNode current;
+   pNode node;
    int aux;
 
-   actual = *a;
-   /* Mientras sea posible que el valor est� en el �rbol */
-   while(!Vacio(actual)) {
-      if(dat == actual->value) { /* Si el valor est� en el nodo actual */
-         if(EsHoja(actual)) { /* Y si adem�s es un nodo hoja: lo borramos */
-            if(padre) /* Si tiene padre (no es el nodo raiz) */
-               /* Anulamos el puntero que le hace referencia */
-               if(padre->right == actual) padre->right = NULL;
-               else if(padre->left == actual) padre->left = NULL;
-            free(actual); /* Borrar el nodo */
-            actual = NULL;
+   current = *tree;
+   
+   //While is possible that the value is in the tree
+   while(!IsEmpty(current)) {
+
+      //The value to delete must be in 
+      //the current node
+      if(data == current->value) { 
+         
+         if(IsLeaf(current)) { 
+
+            //If current is a leaf and has parent,
+            //set the pointers of the parent to null
+            //given the proper case
+            if(parent) {
+
+               if(parent->right == current) 
+                  parent->right = NULL;
+
+               else if(parent->left == current) 
+                  parent->left = NULL;
+            }
+            
+            free(current); 
+            current = NULL;
             return;
          }
-         else { /* Si el valor est� en el nodo actual, pero no es hoja */
-            padre = actual;
-            /* Buscar nodo m�s left de rama derecha */
-            if(actual->right) {
-               nodo = actual->right;
-               while(nodo->left) {
-                  padre = nodo;
-                  nodo = nodo->left;
+         //If current is not leaf...
+         else {
+            parent = current;
+
+            //search the most left node from the right branch
+            if(current->right) {
+               node = current->right;
+               while(node->left) {
+                  parent = node;
+                  node = node->left;
                }
             }
-            /* O buscar nodo m�s right de rama izquierda */
+         
+            //search the most right node from the left branch
             else {
-               nodo = actual->left;
-               while(nodo->right) {
-                  padre = nodo;
-                  nodo = nodo->right;
+               node = current->left;
+               while(node->right) {
+                  parent = node;
+                  node = node->right;
                }
             }
-            /* Intercambiar valores de no a borrar u nodo encontrado
-               y continuar, cerrando el bucle. El nodo encontrado no tiene
-               por qu� ser un nodo hoja, cerrando el bucle nos aseguramos
-               de que s�lo se eliminan nodos hoja. */
-            aux = actual->value;
-            actual->value = nodo->value;
-            nodo->value = aux;
-            actual = nodo;
+
+            //swap with the rightest or leftest node to only delete leafs
+            aux = current->value;
+            current->value = node->value;
+            node->value = aux;
+            current = node;
          }
       }
-      else { /* Todav�a no hemos encontrado el valor, seguir busc�ndolo */
-         padre = actual;
-         if(dat > actual->value) actual = actual->right;
-         else if(dat < actual->value) actual = actual->left;
+      //Otherwise, value has not been found
+      //and it has to be found
+      else { 
+         parent = current;
+
+         if (data > current->value) 
+            current = current->right;
+
+         else if (data < current->value) 
+            current = current->left;
       }
    }
 }
 
-/* Recorrido de �rbol en inorden, aplicamos la funci�n func, que tiene
-   el prototipo:
-   void func(int*);
-*/
-void InOrden(Tree a, void (*func)(int*))
+int Search(Tree tree, int data)
 {
-   if(a->left) InOrden(a->left, func);
-   func(&(a->value));
-   if(a->right) InOrden(a->right, func);
-}
+   pNode current = tree;
 
-/* Recorrido de �rbol en preorden, aplicamos la funci�n func, que tiene
-   el prototipo:
-   void func(int*);
-*/
-void PreOrden(Tree a, void (*func)(int*))
-{
-   func(&a->value);
-   if(a->left) PreOrden(a->left, func);
-   if(a->right) PreOrden(a->right, func);
-}
+   while(!IsEmpty(current)) {
 
-/* Recorrido de �rbol en postorden, aplicamos la funci�n func, que tiene
-   el prototipo:
-   void func(int*);
-*/
-void PostOrden(Tree a, void (*func)(int*))
-{
-   if(a->left) PostOrden(a->left, func);
-   if(a->right) PostOrden(a->right, func);
-   func(&a->value);
-}
+      if(data == current->value) 
+         return TRUE; 
 
-/* Buscar un valor en el �rbol */
-int Buscar(Tree a, int dat)
-{
-   pNode actual = a;
+      else if(data < current->value)
+         current = current->left; 
 
-   /* Todav�a puede aparecer, ya que quedan nodos por mirar */
-   while(!Vacio(actual)) {
-      if(dat == actual->value) return TRUE; /* value encontrado */
-      else if(dat < actual->value) actual = actual->left; /* Seguir */
-      else if(dat > actual->value) actual = actual->right;
+      else if(data > current->value) 
+         current = current->right;
    }
-   return FALSE; /* No est� en �rbol */
+   return FALSE;
 }
 
-/* Calcular la altura del nodo que contiene el value dat */
-int Altura(Tree a, int dat)
-{
-   int altura = 0;
-   pNode actual = a;
 
-   /* Todav�a puede aparecer, ya que quedan nodos por mirar */
-   while(!Vacio(actual)) {
-      if(dat == actual->value) return altura; /* encontrado: devolver altura */
+void InOrder(Tree tree, void (*func)(int*))
+{
+   if(tree->left) 
+      InOrder(tree->left, func);
+   
+   func(&(tree->value));
+
+   if(tree->right) 
+      InOrder(tree->right, func);
+}
+
+void PreOrder(Tree tree, void (*func)(int*))
+{
+   func(&tree->value);
+
+   if(tree->left) 
+      PreOrder(tree->left, func);
+  
+   if(tree->right) 
+      PreOrder(tree->right, func);
+}
+
+void PostOrder(Tree tree, void (*func)(int*))
+{
+   if(tree->left) 
+      PostOrder(tree->left, func);
+   
+   if(tree->right) 
+      PostOrder(tree->right, func);
+   
+   func(&tree->value);
+}
+
+
+int Height(Tree tree, int data)
+{
+   int height = 0;
+   pNode current = tree;
+
+   while(!IsEmpty(current)) { 
+
+      if(data == current->value) 
+         return height; 
+
       else {
-         altura++; /* Incrementamos la altura, seguimos buscando */
-         if(dat < actual->value) actual = actual->left;
-         else if(dat > actual->value) actual = actual->right;
+         height++; 
+
+         if(data < current->value)
+            current = current->left;
+
+         else if(data > current->value) 
+            current = current->right;
       }
    }
-   return -1; /* No est� en �rbol, devolver -1 */
+
+   //The node doesn't exists in the tree
+   return -1; 
 }
 
-/* Contar el n�mero de nodos */
-int NumeroNodos(Tree a, int *contador)
+int NumberOfNodes(Tree tree, int *counter)
 {
-   *contador = 0;
+   *counter = 0;
 
-   auxContador(a, contador); /* Funci�n auxiliar */
-   return *contador;
+   auxCounter(tree, counter); 
+
+   return *counter;
 }
 
-/* Funci�n auxiliar para contar nodos. Funci�n recursiva de recorrido en
-   preorden, el proceso es aumentar el contador */
-void auxContador(Tree nodo, int *c)
+int TreeHeight(Tree tree, int *height)
 {
-   (*c)++; /* Otro nodo */
-   /* Continuar recorrido */
-   if(nodo->left) auxContador(nodo->left, c);
-   if(nodo->right)   auxContador(nodo->right, c);
+   *height = 0;
+
+   auxHeight(tree, 0, height); /* Funci�n auxiliar */
+   return *height;
 }
 
-/* Calcular la altura del �rbol, que es la altura del nodo de mayor altura. */
-int AlturaTree(Tree a, int *altura)
-{
-   *altura = 0;
 
-   auxAltura(a, 0, altura); /* Funci�n auxiliar */
-   return *altura;
+void auxCounter(Tree nodo, int *c)
+{
+   //Pre order
+   (*c)++; 
+   
+   if(nodo->left) 
+      auxCounter(nodo->left, c);
+
+   if(nodo->right)   
+      auxCounter(nodo->right, c);
 }
 
-/* Funci�n auxiliar para calcular altura. Funci�n recursiva de recorrido en
-   postorden, el proceso es actualizar la altura s�lo en nodos hojas de mayor
-   altura de la m�xima actual */
-void auxAltura(pNode nodo, int a, int *altura)
+void auxHeight(pNode node, int a, int *height)
 {
-   /* Recorrido postorden */
-   if(nodo->left) auxAltura(nodo->left, a+1, altura);
-   if(nodo->right)   auxAltura(nodo->right, a+1, altura);
-   /* Proceso, si es un nodo hoja, y su altura es mayor que la actual del
-      �rbol, actualizamos la altura actual del �rbol */
-   if(EsHoja(nodo) && a > *altura) *altura = a;
+   //Post order
+   if(node->left) 
+      auxHeight(node->left, a+1, height);
+
+   if(node->right)   
+      auxHeight(node->right, a+1, height);
+
+   if(IsLeaf(node) && a > *height) *height = a;
 }
 
-/* Comprobar si un �rbol es vac�o */
-int Vacio(Tree r)
+int IsEmpty(Tree tree)
 {
-   return r==NULL;
+   return tree == NULL;
 }
 
-/* Comprobar si un nodo es hoja */
-int EsHoja(pNode r)
+int IsLeaf(pNode node)
 {
-   return !r->right && !r->left;
+   return !node->right && !node->left;
 }
 
-/* Funci�n de prueba para recorridos del �rbol */
-void Mostrar(int *d)
+void Show(int *data)
 {
-   printf("%d, ", *d);
+   printf("%d, ", *data);
 }
 
